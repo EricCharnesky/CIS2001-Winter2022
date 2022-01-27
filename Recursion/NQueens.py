06315d5f597437d7300e98bb716059f33dba0c12
Eric Charnesky <eric.charnesky@careevolution.com>
2022-01-27 16:36:29 -0500

    counting all solutions

--- Recursion/NQueens.py
+++ Recursion/NQueens.py
@@ -4,6 +4,8 @@ class NQueens:
         self._total_queens = number_of_queens
         self._current_number_of_queens = 0
         self._board = []
+        self._number_of_steps = 0
+        self._number_of_solutions = 0
         for row in range(number_of_queens):
             self._board.append([])
             for column in range(number_of_queens):
@@ -12,23 +14,26 @@ class NQueens:
         self.solve()
 
     def __str__(self):
-        return "\n".join(str(row) for row in self._board)
+        return "\n".join(str(row) for row in self._board) + "\n" + "Solution Number: " + str(self._number_of_solutions)
+        #+ "\n" + "Number of Steps: " + str(self._number_of_steps)
 
     def is_solved(self):
         return self._current_number_of_queens == self._total_queens
 
     def solve(self):
         if self.is_solved():
+            self._number_of_solutions += 1
             print(self)
         else:
+            self._number_of_steps += 1
             for row_index in range(self._total_queens):
                 if not self.is_solved() and self._can_place_queen(row_index):
                     self._board[row_index][self._current_number_of_queens] = 'Q'
                     self._current_number_of_queens += 1
                     self.solve()
-                    if not self.is_solved():
-                        self._current_number_of_queens -= 1
-                        self._board[row_index][self._current_number_of_queens] = ' '
+                    # if not self.is_solved(): not only undoing if we found a solution, so we can find ALL
+                    self._current_number_of_queens -= 1
+                    self._board[row_index][self._current_number_of_queens] = ' '
 
     def _is_row_open(self, row_index):
         return 'Q' not in self._board[row_index]
@@ -65,6 +70,11 @@ class NQueens:
 
 
 
-eightQueens = NQueens()
+#for queens in range(4, 21):
+#    print("Queens:", queens)
+#    eightQueens = NQueens(queens)
+#    print()
+
+queens = NQueens()
 
 
